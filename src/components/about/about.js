d06994651a1a07fc5d97fadc0f3ee31639d5348b
Destanee Glenn <destanee.glenn@gmail.com>
2020-12-12 13:59:54 -0600

    adding template and information to app

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -11,23 +11,18 @@ export default class About extends Component {
     </div>
     <div className="nine columns main-col">
       <h2>About Me</h2>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-        eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-        Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+      <p>Hello, my name is Destanee Glenn, a front end developer, currently enrolled in the Coding Bootcamp at the University of Texas Austin. Through my time in the program I will be going from beginner to fullstack developer, completing projects and career services milestones to help guide me. I am currently a third of the way through my course with a graduation date in January of 2021. My goal is to further my career as a webinar coordinator, or wherever my journey takes me. I will be keeping this page updated so you can follow along with me.
       </p>
       <div className="row">
         <div className="columns contact-details">
           <h2>Contact Details</h2>
           <p className="address">
-            <span>Jonathan Doe</span><br />
-            <span>1600 Amphitheatre Parkway<br />
-              Mountain View, CA 94043 US
+            <span>Destanee Glenn</span><br />
+            <span>Austin, TX 78750<br />
+             United States
             </span><br />
-            <span>(123)456-7890</span><br />
-            <span>anyone@website.com</span>
+            <span>(737)224-1230</span><br />
+            <span>destaneelg@github.com</span>
           </p>
         </div>
         <div className="columns download">
